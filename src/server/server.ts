3b4895afce00f378a8309ba8554f184b9b896d11
JhongHawan <brianjhong@outlook.com>
2020-08-26 21:08:03 -0700

    Updated config files

--- src/server/server.ts
+++ src/server/server.ts
@@ -9,8 +9,7 @@ app.get('/', function (req, res) {
 
 });
 
-app.listen(3000, function () {
-
-console.log('App is listening on port 3000!: http://localhost:3000/');
-
-});
+const PORT = process.env.PORT || 3000;
+   app.listen(PORT, () => {
+      console.log(`Server is running in http://localhost:${PORT}`)
+})
