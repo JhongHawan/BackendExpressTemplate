eef30ea13ae6e58d649846da4c9632b6ecd4fdee
JhongHawan <brianjhong@outlook.com>
2020-08-26 20:57:45 -0700

    Setup some biolerplate with typescript

---
+++ src/server/server.ts
@@ -0,0 +1,16 @@
+import express = require('express');
+
+// Create a new express app instance
+const app: express.Application = express();
+
+app.get('/', function (req, res) {
+
+   res.send('Hello World!');
+
+});
+
+app.listen(3000, function () {
+
+console.log('App is listening on port 3000!: http://localhost:3000/');
+
+});
